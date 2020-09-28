8008535e17ba62f9fd69d3ecb817bc2dfad2b556
redawaseem <65788008+redawaseem@users.noreply.github.com>
2020-09-28 12:21:23 -0400

    Updates

---
+++ precache-manifest.7ba564e58fd9b03f917449ae4b81adfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d9437cbc1a3659038bbf1e73bfdc68",
+    "url": "/deployTest/index.html"
+  },
+  {
+    "revision": "69f4eb03dfe5fc64da31",
+    "url": "/deployTest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d5b720cb6ada735e792b",
+    "url": "/deployTest/static/js/2.3bbb2c63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deployTest/static/js/2.3bbb2c63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f4eb03dfe5fc64da31",
+    "url": "/deployTest/static/js/main.be868494.chunk.js"
+  },
+  {
+    "revision": "5d3fa17bcce319132414",
+    "url": "/deployTest/static/js/runtime-main.42720535.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deployTest/static/media/logo.5d5d9eef.svg"
+  }
+]);
